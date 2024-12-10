5eeef53560b54749f800e8c23599fad2c67fb705
0xMithrandir <173684735+0xMithrandir@users.noreply.github.com>
2024-12-10 11:24:39 +0100

    Fix base logo (#83)

--- liquidity/components/icons/BaseIcon/BaseIcon.tsx
+++ liquidity/components/icons/BaseIcon/BaseIcon.tsx
@@ -8,7 +8,7 @@ export const BaseIcon = ({
 }: IconProps) => {
   return (
     <Icon width={width} height={height} viewBox="0 0 24 24" {...props}>
-      <circle cx="12" cy="12" r="12" fill="white" />
+      <circle cx="73" cy="73" r="73" fill="#0052FF" />
       <g width="24" height="24" fill="white">
         <path
           d="M11.9791 24C18.618 24 24 18.6274 24 12C24 5.37257 18.618 0 11.9791 0C5.6804 0 0.513182 4.8359 0 10.9913H15.8889V13.0087H8.6297e-08C0.513182 19.1641 5.6804 24 11.9791 24Z"
