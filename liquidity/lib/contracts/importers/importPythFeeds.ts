a03159542d61fedd3836cdd93893b506a25b90bb
Noisekit <28145325+noisekit@users.noreply.github.com>
2025-01-13 12:58:16 +0800

    Filter out deprecated price feeds (#143)

--- liquidity/lib/contracts/importers/importPythFeeds.ts
+++ liquidity/lib/contracts/importers/importPythFeeds.ts
@@ -1,3 +1,10 @@
+function filterDeprecated(feedId: string) {
+  return ![
+    //
+    '0x5c6c0d2386e3352356c3ab84434fafb5ea067ac2678a38a338c4a69ddc4bdb0c', // FTM
+  ].includes(feedId);
+}
+
 export async function importPythFeeds(chainId?: number, preset?: string): Promise<string[]> {
   if (!preset) {
     throw new Error(`Missing preset`);
@@ -8,43 +15,43 @@ export async function importPythFeeds(chainId?: number, preset?: string): Promis
       const [{ default: pythFeeds }] = await Promise.all([
         import('@synthetixio/v3-contracts/1-main/pythFeeds.json'),
       ]);
-      return pythFeeds;
+      return pythFeeds.filter(filterDeprecated);
     }
     case '11155111-main': {
       const [{ default: pythFeeds }] = await Promise.all([
         import('@synthetixio/v3-contracts/11155111-main/pythFeeds.json'),
       ]);
-      return pythFeeds;
+      return pythFeeds.filter(filterDeprecated);
     }
     case '10-main': {
       const [{ default: pythFeeds }] = await Promise.all([
         import('@synthetixio/v3-contracts/10-main/pythFeeds.json'),
       ]);
-      return pythFeeds;
+      return pythFeeds.filter(filterDeprecated);
     }
     case '8453-andromeda': {
       const [{ default: pythFeeds }] = await Promise.all([
         import('@synthetixio/v3-contracts/8453-andromeda/pythFeeds.json'),
       ]);
-      return pythFeeds;
+      return pythFeeds.filter(filterDeprecated);
     }
     case '84532-andromeda': {
       const [{ default: pythFeeds }] = await Promise.all([
         import('@synthetixio/v3-contracts/84532-andromeda/pythFeeds.json'),
       ]);
-      return pythFeeds;
+      return pythFeeds.filter(filterDeprecated);
     }
     case '42161-main': {
       const [{ default: pythFeeds }] = await Promise.all([
         import('@synthetixio/v3-contracts/42161-main/pythFeeds.json'),
       ]);
-      return pythFeeds;
+      return pythFeeds.filter(filterDeprecated);
     }
     case '421614-main': {
       const [{ default: pythFeeds }] = await Promise.all([
         import('@synthetixio/v3-contracts/421614-main/pythFeeds.json'),
       ]);
-      return pythFeeds;
+      return pythFeeds.filter(filterDeprecated);
     }
     default: {
       throw new Error(`Unsupported deployment ${deployment} for pythFeeds`);
