722ca5950557cd051e4520a8a675ca3db9b7b339
Chase Manning <chase@manning.dev>
2025-04-16 16:46:09 +0100

    update eth contract to new deployment

--- liquidity/lib/contracts/importers/importPositionManagerDeltaNeutralETH.ts
+++ liquidity/lib/contracts/importers/importPositionManagerDeltaNeutralETH.ts
@@ -1,24 +1,19 @@
-const abi = [
-  'function name() view returns (string)',
-  'function symbol() view returns (string)',
-  'function asset() pure returns (address assetTokenAddress)',
-  'function totalSupply() view returns (uint256)',
-  'function totalAssets() view returns (uint256)',
-  'function totalAssetsCap() view returns (uint256)',
-  'function performanceFee() view returns (uint256)',
-  'function exchangeRate() view returns (uint256)',
-  'function deposit(uint256, address)',
-  'function redeem(uint256, address, address)',
-];
+import formatHumanReadableAbi from '../../format/human-readable-abi';
+import fundingRateVaultAbi from '../abis/funding-rate-vault-abi.json';
+
 export async function importPositionManagerDeltaNeutralETH(
   chainId?: number,
   preset?: string
 ): Promise<{ address: string; abi: string[] }> {
   const deployment = `${Number(chainId).toFixed(0)}-${preset}`;
+
   switch (deployment) {
     case '8453-andromeda': {
       // https://basescan.org/address/0x038c33792237F1575136110480142073129bB7e6#code
-      return { address: '0x038c33792237F1575136110480142073129bB7e6', abi };
+      return {
+        address: '0xE51cc84D89b01dD3189458f5bDc69d9877f77119',
+        abi: formatHumanReadableAbi(fundingRateVaultAbi),
+      };
     }
     default: {
       throw new Error(`Unsupported deployment ${deployment} for PositionManagerDeltaNeutralETH`);
