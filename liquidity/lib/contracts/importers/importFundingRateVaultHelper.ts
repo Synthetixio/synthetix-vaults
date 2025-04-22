7e3fab908389fc08986b42ba1d1896eff14229c4
Chase Manning <chase@manning.dev>
2025-04-22 12:27:39 +0100

    update funding rate vault helper

--- liquidity/lib/contracts/importers/importFundingRateVaultHelper.ts
+++ liquidity/lib/contracts/importers/importFundingRateVaultHelper.ts
@@ -1,7 +1,7 @@
 import formatHumanReadableAbi from '@snx-v3/format/human-readable-abi';
 import abi from '../abis/funding-rate-vault-helper-abi.json';
 
-const ADDRESS = '0xd7dB65427e8a80649f057249F681f4c330C4A081';
+const ADDRESS = '0xC9179E7B3BCFc818322A647C1562FCFa6fF272F7';
 
 export async function importFundingRateVaultHelper(
   chainId?: number,
