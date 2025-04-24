c393b40e7dabd1a7ff41bad138102c7b6d3a017b
Chase Manning <chase@manning.dev>
2025-04-24 14:10:31 +0100

    add data to trade history

--- liquidity/lib/Vault/VaultHistory/VaultTradeHistory.tsx
+++ liquidity/lib/Vault/VaultHistory/VaultTradeHistory.tsx
@@ -1,6 +1,19 @@
 import { Table, Th, Thead, Tr, Tbody, Td, Text } from '@chakra-ui/react';
+import {
+  FundingRateVaultData,
+  FundingRateVaultTradeEvent,
+} from '../../useFundingRateVaultData/useFundingRateVaultData';
+import { formatNumberShort, truncateAddress } from '@snx-v3/formatters';
+import { etherscanLink } from '@snx-v3/etherscanLink';
+import { useNetwork } from '@snx-v3/useBlockchain';
 
-export const VaultTradeHistory = () => {
+interface Props {
+  vaultData: FundingRateVaultData;
+}
+
+export const VaultTradeHistory = ({ vaultData }: Props) => {
+  const { network } = useNetwork();
+
   return (
     <Table>
       <Thead whiteSpace="nowrap">
@@ -28,7 +41,7 @@ export const VaultTradeHistory = () => {
             lineHeight="16px"
             fontWeight={400}
           >
-            Venue
+            From
           </Th>
 
           <Th
@@ -41,7 +54,7 @@ export const VaultTradeHistory = () => {
             lineHeight="16px"
             fontWeight={400}
           >
-            Trade Value
+            To
           </Th>
           <Th
             py={2}
@@ -53,7 +66,19 @@ export const VaultTradeHistory = () => {
             lineHeight="16px"
             fontWeight={400}
           >
-            Fees
+            Amount In
+          </Th>
+          <Th
+            py={2}
+            textTransform="unset"
+            color="gray.600"
+            border="none"
+            fontFamily="heading"
+            fontSize="12px"
+            lineHeight="16px"
+            fontWeight={400}
+          >
+            Amount Out
           </Th>
           <Th
             py={2}
@@ -81,42 +106,39 @@ export const VaultTradeHistory = () => {
           <Td height="0px" border="none" px={0} pt={0} pb={0} />
         </Tr>
 
-        <Tr>
-          <Td border="none" fontSize="12px" fontWeight={400} py={2}>
-            02/24/2025
-            <Text textColor="gray.500">14:03:21</Text>
-          </Td>
-          <Td border="none" fontSize="12px" fontWeight={400} py={2}>
-            Synthetix Perps v3
-          </Td>
-          <Td border="none" fontSize="12px" fontWeight={400} py={2}>
-            $1,200.0232
-          </Td>
-          <Td border="none" fontSize="12px" fontWeight={400} py={2}>
-            $0.3234
-          </Td>
-          <Td textDecoration="underline" border="none" fontSize="12px" fontWeight={400} py={2}>
-            0x46f...ec8fc
-          </Td>
-        </Tr>
-        <Tr>
-          <Td border="none" fontSize="12px" fontWeight={400} py={2}>
-            02/24/2025
-            <Text textColor="gray.500">14:03:21</Text>
-          </Td>
-          <Td border="none" fontSize="12px" fontWeight={400} py={2}>
-            0x46f...ec8fc
-          </Td>
-          <Td border="none" fontSize="12px" fontWeight={400} py={2}>
-            $1,200.0232
-          </Td>
-          <Td border="none" fontSize="12px" fontWeight={400} py={2}>
-            -
-          </Td>
-          <Td textDecoration="underline" border="none" fontSize="12px" fontWeight={400} py={2}>
-            0x46f...ec8fc
-          </Td>
-        </Tr>
+        {vaultData.trades.map((trade: FundingRateVaultTradeEvent) => (
+          <Tr key={trade.transactionHash}>
+            <Td border="none" fontSize="12px" fontWeight={400} py={2}>
+              {trade.timestamp.toLocaleDateString()}
+              <Text textColor="gray.500">{trade.timestamp.toLocaleTimeString()}</Text>
+            </Td>
+            <Td border="none" fontSize="12px" fontWeight={400} py={2}>
+              {trade.fromSymbol}
+            </Td>
+            <Td border="none" fontSize="12px" fontWeight={400} py={2}>
+              {trade.toSymbol}
+            </Td>
+            <Td border="none" fontSize="12px" fontWeight={400} py={2}>
+              {formatNumberShort(trade.amountIn)}
+            </Td>
+            <Td border="none" fontSize="12px" fontWeight={400} py={2}>
+              {formatNumberShort(trade.amountOut)}
+            </Td>
+            <Td textDecoration="underline" border="none" fontSize="12px" fontWeight={400} py={2}>
+              <a
+                href={etherscanLink({
+                  chain: network?.name || '',
+                  address: trade.transactionHash,
+                  isTx: true,
+                })}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {truncateAddress(trade.transactionHash)}
+              </a>
+            </Td>
+          </Tr>
+        ))}
       </Tbody>
     </Table>
   );
