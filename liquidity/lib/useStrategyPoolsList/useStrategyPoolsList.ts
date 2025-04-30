dbcede886b9e6aa49cfd30684e32ad8682dbf853
Chase Manning <chase@manning.dev>
2025-04-30 10:53:54 +0100

    add btc fundiung rate vault to home page

--- liquidity/lib/useStrategyPoolsList/useStrategyPoolsList.ts
+++ liquidity/lib/useStrategyPoolsList/useStrategyPoolsList.ts
@@ -1,5 +1,5 @@
 import { BASE_ANDROMEDA, useNetwork } from '@snx-v3/useBlockchain';
-// import { usePositionManagerDeltaNeutralBTC } from '../contracts/usePositionManagerDeltaNeutralBTC';
+import { usePositionManagerDeltaNeutralBTC } from '../contracts/usePositionManagerDeltaNeutralBTC';
 import { usePositionManagerDeltaNeutralETH } from '../contracts/usePositionManagerDeltaNeutralETH';
 import { useMemo } from 'react';
 import { FundingRateVaultData, useFundingRateVaultData } from '../useFundingRateVaultData';
@@ -9,15 +9,15 @@ export function useStrategyPoolsList(): FundingRateVaultData[] | undefined {
   const targetNetwork = network || BASE_ANDROMEDA;
 
   const { data: DeltaNeutralETH } = usePositionManagerDeltaNeutralETH(targetNetwork);
-  // const { data: DeltaNeutralBTC } = usePositionManagerDeltaNeutralBTC(targetNetwork);
+  const { data: DeltaNeutralBTC } = usePositionManagerDeltaNeutralBTC(targetNetwork);
 
-  // const { data: btcData } = useFundingRateVaultData(DeltaNeutralBTC?.address);
+  const { data: btcData } = useFundingRateVaultData(DeltaNeutralBTC?.address);
   const { data: ethData } = useFundingRateVaultData(DeltaNeutralETH?.address);
 
   return useMemo(() => {
-    if (!ethData) {
+    if (!ethData || !btcData) {
       return undefined;
     }
-    return [ethData];
-  }, [ethData]);
+    return [ethData, btcData];
+  }, [ethData, btcData]);
 }
