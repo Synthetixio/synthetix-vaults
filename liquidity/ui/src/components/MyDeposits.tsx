463c66e7851d43192a5d2846c491e32b494141ab
Chase Manning <chase@manning.dev>
2025-05-22 16:07:34 +0100

    remove redundant code

--- liquidity/ui/src/components/MyDeposits.tsx
+++ liquidity/ui/src/components/MyDeposits.tsx
@@ -4,11 +4,8 @@ import { StatsBox } from '@snx-v3/StatsBox';
 import { useWallet } from '@snx-v3/useBlockchain';
 import { useLiquidityPositions } from '@snx-v3/useLiquidityPositions';
 import { useParams } from '@snx-v3/useParams';
-import { useStrategyPoolsList } from '@snx-v3/useStrategyPoolsList';
-import Wei, { wei } from '@synthetixio/wei';
+import { wei } from '@synthetixio/wei';
 import React from 'react';
-import { FundingRateVaultData } from '../../../lib/useFundingRateVaultData';
-import { BigNumber } from 'ethers';
 
 export function MyDeposits() {
   const [params] = useParams();
@@ -18,8 +15,6 @@ export function MyDeposits() {
     useLiquidityPositions({
       accountId: params.accountId,
     });
-
-  const strategyPoolsList = useStrategyPoolsList();
 
   const lpTotalLocked = React.useMemo(
     () =>
@@ -32,31 +27,12 @@ export function MyDeposits() {
         : wei(0),
     [liquidityPositions]
   );
-  const fundingRateTotalLocked = React.useMemo(
-    () =>
-      strategyPoolsList
-        ? strategyPoolsList.reduce(
-            (result: Wei, strategyPool: FundingRateVaultData) =>
-              result.add(
-                strategyPool.balanceOf
-                  .mul(strategyPool.exchangeRate)
-                  .div(BigNumber.from(10).pow(18))
-              ),
-            wei(0)
-          )
-        : wei(0),
-    [strategyPoolsList]
-  );
-
-  const totalLocked = React.useMemo(() => {
-    return lpTotalLocked.add(fundingRateTotalLocked);
-  }, [lpTotalLocked, fundingRateTotalLocked]);
 
   return (
     <StatsBox
       title="Total Value of My Deposits"
       isLoading={!(!params.accountId || (params.accountId && !isPendingLiquidityPositions))}
-      value={activeWallet && totalLocked ? <Amount prefix="$" value={totalLocked} /> : '-'}
+      value={activeWallet && lpTotalLocked ? <Amount prefix="$" value={lpTotalLocked} /> : '-'}
       label={
         <>
           <Text textAlign="left">All assets locked in positions</Text>
